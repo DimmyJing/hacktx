70f322793224a6d2285bd302de9ba7a2ba51bd3a
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2023-10-22 00:36:13 -0500

    Add add/remove friend endpoints

--- api/firebase.py
+++ api/firebase.py
@@ -3,6 +3,8 @@ import firebase_admin
 from firebase_admin import credentials, auth
 from pymongo import MongoClient
 from dotenv import dotenv_values
+from pydantic import BaseModel
+
 # among us
 config = dotenv_values(".env")
 app = FastAPI()
@@ -19,10 +21,10 @@ def read_root():
 @ app.get("/{id_token}")
 def verify_id(id_token: str):
     decoded_token = auth.verify_id_token(id_token)
-    uid = decoded_token['uid']
+    uuid = decoded_token['uid']
     # upsert to the database with the user info
     # update the last login time
-    return uid
+    return uuid
 
 @ app.get("/user/{uuid}")
 def get_user(uuid: str):
@@ -34,7 +36,24 @@ def get_friends(uuid: str):
     user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
     return list(map(get_user, user['friends']))
 
+class FriendsPostReq(BaseModel):
+    idToken: str
+    friendUuid: str
 
+@ app.post("/friends/add")
+def add_friend(req: FriendsPostReq):
+    uuid = verify_id(req.idToken)
+    user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
+    if req.friendUuid in user['friends']:
+        return 0
+    result = app.database.users.update_one({"uuid": uuid}, {"$push": {"friends": req.friendUuid}})
+    return result.modified_count
+
+@ app.post("/friends/remove")
+def remove_friend(req: FriendsPostReq):
+    uuid = verify_id(req.idToken)
+    result = app.database.users.update_one({"uuid": uuid}, {"$pull": {"friends": req.friendUuid}})
+    return result.modified_count
 
 @ app.on_event("startup")
 def startup_db_client():
@@ -45,5 +64,3 @@ def startup_db_client():
 @ app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
-
-@ app.get("/")
