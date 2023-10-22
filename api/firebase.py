1ca8c35f3bf9e8707dbe9402cfa33c25ba9174e2
Kevin Seon <apkpj@wireless-10-145-65-13.public.utexas.edu>
2023-10-22 02:20:34 -0500

    .

--- api/firebase.py
+++ api/firebase.py
@@ -14,8 +14,8 @@ firebase_admin.initialize_app(cred)
 
 def verify_id(id_token: str):
     decoded_token = auth.verify_id_token(id_token)
-    uuid = decoded_token['uid']
-    return uuid
+    uid = decoded_token['uid']
+    return uid
 
 @ app.get("/")
 def read_root():
@@ -32,7 +32,7 @@ def shutdown_db_client():
     app.mongodb_client.close()
 
 class CreateUserPostReq(BaseModel):
-    uuid: str
+    uid: str
     name: str
     email: str
     avatar: str
@@ -40,7 +40,7 @@ class CreateUserPostReq(BaseModel):
 @ app.post("/create-user")
 def create_user(req: CreateUserPostReq):
     app.database.users.insert_one({
-        "uuid": req.uuid,
+        "uid": req.uid,
         "lastLogin": datetime.datetime.now(),
         "name": req.name,
         "email": req.email,
@@ -51,14 +51,14 @@ def create_user(req: CreateUserPostReq):
 
 @ app.get("/user/{token}")
 def get_user(token: str):
-    uuid = verify_id(token)
-    user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
+    uid = verify_id(token)
+    user = app.database.users.find_one({"uid": uid}, {'_id': 0})
     return user
 
 @ app.get("/friends/{token}")
 def get_friends(token: str):
-    uuid = verify_id(token)
-    user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
+    uid = verify_id(token)
+    user = app.database.users.find_one({"uid": uid}, {'_id': 0})
     return list(map(get_user, user['friends']))
 
 class FriendsPostReq(BaseModel):
@@ -67,45 +67,49 @@ class FriendsPostReq(BaseModel):
 
 @ app.post("/friends/add")
 def add_friend(req: FriendsPostReq):
-    uuid = verify_id(req.token)
-    friendUuid = verify_id(req.friendtoken)
-    user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
-    if friendUuid in user['friends']:
+    uid = verify_id(req.token)
+    frienduid = verify_id(req.friendtoken)
+    user = app.database.users.find_one({"uid": uid}, {'_id': 0})
+    if frienduid in user['friends']:
         return 0
-    result = app.database.users.update_one({"uuid": uuid}, {"$push": {"friends": friendUuid}})
+    result = app.database.users.update_one({"uid": uid}, {"$push": {"friends": frienduid}})
     return result.modified_count
 
 @ app.post("/friends/remove")
 def remove_friend(req: FriendsPostReq):
-    uuid = verify_id(req.token)
-    friendUuid = verify_id(req.friendtoken)
-    result = app.database.users.update_one({"uuid": uuid}, {"$pull": {"friends": friendUuid}})
+    uid = verify_id(req.token)
+    frienduid = verify_id(req.friendtoken)
+    result = app.database.users.update_one({"uid": uid}, {"$pull": {"friends": frienduid}})
     return result.modified_count
 
 # ============================
 
 class Message(BaseModel):
     token1: str
-    token2: str
+    uid2: str
     message: str
 
-# uuid1 to uuid2
+# uid1 to uid2
 @ app.post("/send_message")
 def post_chat(message: Message):
-    uuid1 = verify_id(message.token1)
-    temp1, temp2 = sorted([verify_id(message.token1), verify_id(message.token2)])
+    uid1 = verify_id(message.token1)
+    temp1, temp2 = sorted([verify_id(message.token1), message.uid2])
     message = message.message
 
-    # from uuid1 to uuid2
+    # from uid1 to uid2
     now = datetime.datetime.now()
-    app.database.chats.update_one({"uuid1": temp1, "uuid2": temp2}, {"$push": {"log": [str(now), uuid1 == temp1, message]}}, upsert=True)
+    app.database.chats.update_one({"uid1": temp1, "uid2": temp2}, {"$push": {"log": [str(now), uid1 == temp1, message]}}, upsert=True)
     return message
 
-@ app.get("/get_chat/{token1}/{token2}")
-def get_chat(token1: str, token2: str):
-    uuid1 = verify_id(token1)
-    temp1, temp2 = sorted([verify_id(token1), verify_id(token2)])
-    chat = app.database.chats.find_one({"uuid1": temp1, "uuid2": temp2}, {'_id': 0})
-    if uuid1 != temp1:
+@ app.get("/get_chat/{token1}/{uid2}")
+def get_chat(token1: str, uid2: str):
+    uid1 = verify_id(token1)
+    temp1, temp2 = sorted([verify_id(token1), uid2])
+    print(temp1 + 'temp1')
+    print(temp2 + 'temp2')
+    chat = app.database.chats.find_one({"uid1": temp1, "uid2": temp2}, {'_id': 0})
+    print(chat)
+    if uid1 != temp1:
         chat['log'] = list(map(lambda x: [x[0], not x[1], x[2]], chat['log']))
     return chat['log'] if chat else []
+
