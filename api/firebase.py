bfb72e7dc8b659b8ff665fd7b262838ec83a149b
Kevin Seon <apkpj@wireless-10-147-49-194.public.utexas.edu>
2023-10-21 22:25:20 -0500

    .

--- api/firebase.py
+++ api/firebase.py
@@ -20,20 +20,20 @@ def read_root():
 def verify_id(id_token: str):
     decoded_token = auth.verify_id_token(id_token)
     uid = decoded_token['uid']
+    # upsert to the database with the user info
     # update the last login time
-    return {uid}
-
+    return uid
 
 @ app.get("/user/{uuid}")
 def get_user(uuid: str):
     user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
     return user
 
-
 @ app.get("/friends/{uuid}")
 def get_friends(uuid: str):
     user = app.database.users.find_one({"uuid": uuid}, {'_id': 0})
     return list(map(get_user, user['friends']))
+
 
 
 @ app.on_event("startup")
@@ -42,7 +42,8 @@ def startup_db_client():
     app.database = app.mongodb_client[config["DB_NAME"]]
     print("Connected to the MongoDB database!!")
 
-
 @ app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
+
+@ app.get("/")
